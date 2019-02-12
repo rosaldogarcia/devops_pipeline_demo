9f429b4988dd0c30f26877d86b92eb504cbcdee2
ITG Core Infrastructure Systems (RGarcia) <rggarcia@securitybank.com.ph>
2019-02-12 20:02:37 +0800

    first commit

--- java_web_code/src/main/java/de/vorb/wildfly_springboot/SampleResource.java
+++ java_web_code/src/main/java/de/vorb/wildfly_springboot/SampleResource.java
@@ -8,7 +8,7 @@ public class SampleResource {
 
     @RequestMapping("/")
     public String hello() {
-        return "Hello World! - Ross";
+        return "Hello World! - feb.12,2019";
     }
 
 }
